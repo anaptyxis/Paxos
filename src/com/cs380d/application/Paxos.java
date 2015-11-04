67d89a1b3d2f0d069d48dc007a4c9fb5fa5857d9
Zhang Tian <zhangtian1000@gmail.com>
2015-11-04 10:15:31 -0600

    update client sending function

--- src/com/cs380d/application/Paxos.java
+++ src/com/cs380d/application/Paxos.java
@@ -92,6 +92,12 @@ public class Paxos {
 		 */
 		
 		public void clientBroadcast(int client, String Msg ){
+			  try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			  clientList[client].broadcast(Msg);
 			  
 		}
